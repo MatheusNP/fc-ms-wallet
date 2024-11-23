22f31c774b5049117ead2cc278ad769eb162c200
Matheus Nobre <matheusnobre56@gmail.com>
2024-11-23 18:19:55 -0300

    feat: client db

--- internal/database/client_db_test.go
+++ internal/database/client_db_test.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/MatheusNP/fc-ms-wallet/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -19,7 +21,7 @@ func (s *ClientDBTestSuite) SetupTest() {
 
 	s.db = db
 
-	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date, updated_at date)")
+	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date, updated_at date default current_timestamp)")
 
 	s.clientDB = NewClientDB(db)
 }
@@ -32,4 +34,28 @@ func (s *ClientDBTestSuite) TearDownTest() {
 
 func TestClientDBTestSuite(t *testing.T) {
 	suite.Run(t, new(ClientDBTestSuite))
+}
+
+func (s *ClientDBTestSuite) TestGet() {
+	client, _ := entity.NewClient("name", "email")
+
+	err := s.clientDB.Save(client)
+	s.Nil(err)
+
+	clientFound, err := s.clientDB.Get(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientFound.ID)
+	s.Equal(client.Name, clientFound.Name)
+	s.Equal(client.Email, clientFound.Email)
+}
+
+func (s *ClientDBTestSuite) TestSave() {
+	client := &entity.Client{
+		ID:    "123",
+		Name:  "name",
+		Email: "email",
+	}
+
+	err := s.clientDB.Save(client)
+	s.Nil(err)
 }
